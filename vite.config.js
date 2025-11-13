2db09b4365b096009e4307924f8ebd44ebe9ace5
ahmed159159 <111837367+ahmed159159@users.noreply.github.com>
2025-11-13 11:57:14 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/MOVIESBOX/",   // أهم سطر لمنع الشاشة السوداء من GitHub Pages
   plugins: [react(), tailwindcss()],
 });
